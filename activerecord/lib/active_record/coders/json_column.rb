aea98a3d59f7c181c0fe63c8ccb9e99578bf3cca
Jenn Cooper <cooper.jennl@gmail.com>
2014-07-04 10:01:13 -0700

    move alias below delegation

--- activerecord/lib/active_record/coders/json_column.rb
+++ activerecord/lib/active_record/coders/json_column.rb
@@ -1,10 +1,16 @@
 module ActiveRecord
   module Coders # :nodoc:
     class JSONColumn # :nodoc:
-      alias_method :dump, :encode
-      alias_method :load, :decode
+      attr_accessor :object_class
+
       delegate :decode, to: ActiveSupport::JSON
       delegate :encode, to: ActiveSupport::JSON
+      alias_method :dump, :encode
+      alias_method :load, :decode
+
+      def initialize(object_class = Object)
+        @object_class = object_class
+      end
     end
   end
 end
