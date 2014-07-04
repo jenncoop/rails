26776be7ce12d5f9b9c3890ed2f65e99cf72ca13
Jenn Cooper <cooper.jennl@gmail.com>
2014-07-04 10:36:12 -0700

    we need a space before # :nodoc: to get it to work correctly

--- activerecord/lib/active_record/coders/json.rb
+++ activerecord/lib/active_record/coders/json.rb
@@ -1,6 +1,6 @@
 module ActiveRecord
   module Coders # :nodoc:
-    class JSON# :nodoc:
+    class JSON # :nodoc:
       delegate :decode, to: ActiveSupport::JSON
       delegate :encode, to: ActiveSupport::JSON
       alias_method :dump, :encode
